07a92233d3e76d7501817a5dacd4e4a79a463b57
Gabriel Sousa <gabrielsousa.ti@gmail.com>
2021-02-03 16:59:20 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,6 +17,8 @@ public class Program {
 		Seller seller = sellerDao.findById(3);		
 		System.out.println(seller);
 		
+		System.out.println();
+		
 		System.out.println("=== TEST 2: seller findByDep ===");
 		Department dep = new Department(2, null);
 		List<Seller> sellers = sellerDao.findByDepartment(dep);
@@ -24,16 +26,31 @@ public class Program {
 			System.out.println(seller1);
 		}
 		
+		System.out.println();
+		
 		System.out.println("=== TEST 3: seller findAll ===");
 		sellers = sellerDao.findAll();
 		for(Seller seller1: sellers) {
 			System.out.println(seller1);
 		}
 		
+		System.out.println();
+		
 		System.out.println("=== TEST 4: seller Insert ===");
-		Seller newSeller = new Seller(null, "Gabriel", "gabrielsousa@snews.tv", new Date(), 2696.0 , dep);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = "+ newSeller.getId());
+		/*
+		 * Seller newSeller = new Seller(null, "Gabriel", "gabrielsousa@snews.tv", new
+		 * Date(), 2696.0 , dep); sellerDao.insert(newSeller);
+		 * System.out.println("Inserted! New id = "+ newSeller.getId());
+		 * 
+		 * System.out.println();
+		 */
+		
+		System.out.println("=== TEST 5: seller Update ===");
+		seller = sellerDao.findById(7);
+		seller.setName("Gabriel Oliveira");
+		seller.setBaseSalary(9995.0);
+		sellerDao.update(seller);
+		System.out.println("Atualizado! "+ seller);
 
 	}
 }
